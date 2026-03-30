842b966e64e0df8dae9027881cff03da38cf5d32
Zozoro Zady <zadi.zozoro@gmail.com>
2026-03-30 07:59:09 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
-import { StrictMode } from "react";
+importimport { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-createRoot(document.getElementById("root")).render(<StrictMode><App /></StrictMode>);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
